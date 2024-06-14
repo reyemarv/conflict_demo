68f5504df48e5f039ba29c1d84214e62d69d0fc8
Marvin Reyes <reyemarv@gmail.com>
2024-06-14 18:04:24 -0500

    merge

--- helloworld.py
+++ helloworld.py
@@ -1,21 +1,9 @@
-<<<<<<< HEAD
 def hello_world(city, state):
     print(f'Hello World {city}, {state}!')
 
 
 def main():
     hello_world('NYC', 'New York')
-
-
-main()
-=======
-def hello_world(city):
-    print(f'Hello World {city}!')
-
-
-def main():
-    hello_world('NYC')
->>>>>>> 3338c653eec09a350ebdfaf05a7247a9067b23c7
 
 
 main()
